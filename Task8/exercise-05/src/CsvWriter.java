93cfe8759292b934324cc2f069d128ae6bbde540
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2020-12-06 16:36:17 +0200

    changed task 5

--- Task8/exercise-05/src/CsvWriter.java
+++ Task8/exercise-05/src/CsvWriter.java
@@ -3,22 +3,24 @@ public class CsvWriter {
 	}
 
 	public void write(String[][] lines) {
-		for (int i = 0; i < lines.length; i++)
-			writeLine(lines[i]);
+		for (String[] line : lines){
+			writeLine(line);
+		}
 	}
 
 	private void writeLine(String[] fields) {
-		if (fields.length == 0)
+		if (fields.length == 0){
 			System.out.println();
-		else {
-			writeField(fields[0]);
-
-			for (int i = 1; i < fields.length; i++) {
-				System.out.print(",");
-				writeField(fields[i]);
-			}
-			System.out.println();
+			return;
 		}
+		writeField(fields[0]);
+
+		for (int i = 1; i < fields.length; i++) {
+			System.out.print(",");
+			writeField(fields[i]);
+		}
+		System.out.println();
+
 	}
 
 	private void writeField(String field) {
@@ -30,12 +32,11 @@ public class CsvWriter {
 
 	private void writeQuoted(String field) {
 		System.out.print('\"');
-		for (int i = 0; i < field.length(); i++) {
-			char c = field.charAt(i);
-			if (c == '\"')
+		for (char item : field.toCharArray()){
+			if (item == '\"')
 				System.out.print("\"\"");
 			else
-				System.out.print(c);
+				System.out.print(item);
 		}
 		System.out.print('\"');
 	}
